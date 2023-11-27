587ecde0a0fdb9a0815ee5c10fe22064f897f2ff
Nico Dirkes <dirkes@aices.rwth-aachen.de>
2023-11-27 19:02:54 +0100

    update version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='hemtracer',
-    version='0.1.0',
+    version='0.2.0',
     description='Lagrangian hemolysis post-processing for Eulerian CFD simulations',
     long_description=readme,
     author='Nico Dirkes',
